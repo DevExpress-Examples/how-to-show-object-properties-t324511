b624d2334305a04e2aeb750d34b833ce7a48ece5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Data.cs
@@ -0,0 +1,138 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace DXSample {
+    public class ViewModel {
+        public List<Contact> Items { get; set; }
+        public ViewModel() {
+            Items = new List<Contact>() {
+                new Contact() {
+                    FirstName = "Carolyn",
+                    LastName = "Baker",
+                    Email = "carolyn.baker@example.com", 
+                    Phone = "(555)349-3010",
+                    Address = "1198 Theresa Cir", 
+                    City = "Whitinsville", 
+                    State = "MA", 
+                    Zip = "01582"
+                },
+                new Contact() {
+                    FirstName = "Amber",
+                    LastName = "Seaman",
+                    Phone = "(555)698-4285",
+                    Address = "28700 S Main St", 
+                    City = "Wilsonville", 
+                    State = "AL", 
+                    Zip = "35188"
+                },
+                new Contact() {
+                    FirstName = "Annie",
+                    LastName = "Vicars",
+                    Email = "annie.vicars@example.com", 
+                    Phone = "(555)922-1349",
+                    Address = "7267 New York Ave", 
+                    City = "Jersey City", 
+                    State = "NJ", 
+                    Zip = "07306"
+                },
+                new Contact() {
+                    FirstName = "Darlene",
+                    LastName = "Catto",
+                    Email = "darlene.catto@example.com", 
+                    Phone = "(555)752-0582",
+                    Address = "32125 4th St NW #210", 
+                    City = "Washington", 
+                    State = "DC", 
+                    Zip = "20001"
+                },
+                new Contact() {
+                    FirstName = "Angela",
+                    LastName = "Gross",
+                    Email = "angela.gross@example.com", 
+                    Phone = "(555)247-1252",
+                    Address = "8723 Chicago Ave", 
+                    City = "Nederland", 
+                    State = "TX", 
+                    Zip = "77620"
+                }
+            };
+        }
+    }
+    public class Contact : BindableBase {
+        string _FirstName;
+        public string FirstName {
+            get { return _FirstName; }
+            set {
+                _FirstName = value;
+                RaisePropertyChanged(() => FirstName);
+            }
+        }
+
+        string _LastName;
+        public string LastName {
+            get { return _LastName; }
+            set {
+                _LastName = value;
+                RaisePropertyChanged(() => LastName);
+            }
+        }
+
+        string _Email;
+        public string Email {
+            get { return _Email; }
+            set {
+                _Email = value;
+                RaisePropertyChanged(() => Email);
+            }
+        }
+
+        string _Phone;
+        public string Phone {
+            get { return _Phone; }
+            set {
+                _Phone = value;
+                RaisePropertyChanged(() => Phone);
+            }
+        }
+
+        string _Address;
+        public string Address {
+            get { return _Address; }
+            set {
+                _Address = value;
+                RaisePropertyChanged(() => Address);
+            }
+        }
+
+        string _City;
+        public string City {
+            get { return _City; }
+            set {
+                _City = value;
+                RaisePropertyChanged(() => City);
+            }
+        }
+
+        string _State;
+        public string State {
+            get { return _State; }
+            set {
+                _State = value;
+                RaisePropertyChanged(() => State);
+            }
+        }
+
+        string _Zip;
+        public string Zip {
+            get { return _Zip; }
+            set {
+                _Zip = value;
+                RaisePropertyChanged(() => Zip);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,50 @@
+# How to: Show Object Properties
+
+
+<p>The PropertyGridControl is the control designed to edit object properties. It can be bound to a specific object as well as a collection of objects.</p><br><p>To browse and manage properties of a specific object, set the  <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfPropertyGridPropertyGridControl_SelectedObjecttopic">PropertyGridControl.SelectedObject</a> property to this object.</p>
+
+```cs
+public partial class MainWindow {
+    public MainWindow() {
+        DataContext = new Contact("Carolyn", "Baker");
+        InitializeComponent();
+    }
+}
+public class Contact {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Contact(string firstName, string lastName) {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
+```
+
+
+
+```xaml
+<dxprg:PropertyGridControl SelectedObject="{Binding Path=.}"/>
+```
+
+<br><p>To edit properties of multiple objects simultaneously, use <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfPropertyGridPropertyGridControl_SelectedObjectstopic">PropertyGridControl.SelectedObjects</a>.</p>
+
+```cs
+public partial class MainWindow {
+    public MainWindow() {
+        DataContext = new List<Contact> { new Contact("Carolyn", "Baker"), new Contact("Amber", "Seaman") };
+        InitializeComponent();
+    }
+}
+```
+
+
+
+```xaml
+<dxprg:PropertyGridControl SelectedObjects="{Binding Path=.}" ExpandCategoriesWhenSelectedObjectChanged="True"/>
+```
+
+<br>
+
+<br/>
+
+
